059e41ec3d8956d6c578cf78209cb4240c7703ff
17446457 <piskunov.p.v@sber.ru>
2023-05-21 23:52:27 +0300

    Выполнить рефакторинг:  - логирования ErrorHandle, включить @Slf4j  - application.properties, включить logging.level.ru.yandex.practicum=debug  - добавить time в ErrorResponse(помогает при отслеживании ошибок, к примеру, в graylog, когда разные TimeZone'ы или разъехался NTP)  - убрать лишний вызов getFilms, в FilmController  - убрать излишние проверки checkUserId, checkFilmId в FilmDbStorage  - Убрать излишнюю проверку checkUserById, в UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/exception/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/handler/ErrorHandler.java
@@ -1,40 +1,49 @@
 package ru.yandex.practicum.filmorate.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.*;
 
+import java.util.Arrays;
+
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
+        log.error(Arrays.toString(e.getStackTrace()));
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleUserNotFoundException(final UserNotFoundException e) {
+        log.error(Arrays.toString(e.getStackTrace()));
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleFilmNotFoundException(final FilmNotFoundException e) {
+        log.error(Arrays.toString(e.getStackTrace()));
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleGenreNotFoundException(final GenreNotFoundException e) {
+        log.error(Arrays.toString(e.getStackTrace()));
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleRatingNotFoundException(final RatingNotFoundException e) {
+        log.error(Arrays.toString(e.getStackTrace()));
         return new ErrorResponse(e.getMessage());
     }
 
@@ -47,6 +56,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowableException(final Throwable e) {
-        return new ErrorResponse("Unexpected error occurred.");
+        log.error(Arrays.toString(e.getStackTrace()));
+        return new ErrorResponse(e.getMessage());
     }
 }
